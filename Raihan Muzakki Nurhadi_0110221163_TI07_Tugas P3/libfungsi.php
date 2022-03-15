38b3c3fbcfa4ef056faf11bf6731ab08e8f7052f
Raihan Muzakki Nurhadi <raihanmuzakki.rm@gmail.comgit config --global user.email raihanmuzakki.rm@gmail.comgit config --global user.email raihanmuzakki.rm@gmail.com>
2022-03-15 10:58:45 +0700

    P3

---
+++ Raihan Muzakki Nurhadi_0110221163_TI07_Tugas P3/libfungsi.php
@@ -0,0 +1,79 @@
+<?php
+$_proses = $_POST['proses'];
+$_nama = $_POST['nama'];
+$_matkul = $_POST['matkul'];
+function matkul($_namaMatkul){
+    switch ($_namaMatkul) {
+        case 'DDP':
+          $nama_matkul = 'DDP'; 
+          break;
+        case 'PEMWEB':
+          $nama_matkul = 'PEMWEB'; 
+          break;
+        case 'Jaringan Komputer':
+          $nama_matkul = 'Jaringan Komputer'; 
+          break;    
+        default:"";
+      }
+      return $nama_matkul;
+}
+    $_proses = $_POST['proses'];
+    $_nilaiUas = $_POST['nilai_uas'];
+    $_uts = $_POST['nilai_uts'];
+    $_uas = $_POST['nilai_uas'];
+    $_tugas = $_POST['nilai_tugas'];
+    $nilai_akhir = ($_uts + $_tugas + $_uas)/3;
+
+function kelulusan($_nilai)
+{ 
+    if ($_nilai <= 55) {
+        $pernyataan = 'Tidak Lulus';
+    } else {
+        $pernyataan = 'Lulus';
+    }
+    return $pernyataan;
+}
+
+function predikat($nilai_akhir){
+    if ($nilai_akhir <= 35) {
+        $grade = "E";
+    } elseif($nilai_akhir >= 36 && $nilai_akhir <= 55) {
+        $grade = "D";
+    } elseif($nilai_akhir >= 56 && $nilai_akhir <= 69) {
+        $grade = "C";
+    } elseif($nilai_akhir >= 70 && $nilai_akhir <= 84) {
+        $grade = "B";
+    } elseif($nilai_akhir >= 85 && $nilai_akhir <= 100) {
+        $grade = "A";
+    } else{
+        echo "Invalid";
+    }
+    return $grade;
+}
+
+function grade($grade){
+    switch ($grade) {
+        case('A') :
+            $predikat = "Sangat Memuaskan";
+            break;
+        case ('B'):
+            $predikat = "Memuaskan"; 
+            break;
+        case ('C'):
+            $predikat = "Cukup"; 
+            break;
+        case ('D'):
+            $predikat = "Kurang"; 
+            break;
+        case ('E'):
+            $predikat = "Sangat Kurang"; 
+            break;
+        case ('I'):
+            $predikat = "Tidak Ada"; 
+            break;
+        default:
+            echo "Invalid";
+    }
+    return $predikat;
+}
+?>
